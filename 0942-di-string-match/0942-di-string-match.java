8fa065d03a0d5bfc61aaff35d97eac28411a4fe6
JotheeshKumar <80279461+jo73na@users.noreply.github.com>
2024-01-07 16:59:07 -0500

    Time: 3 ms (63.47%), Space: 44.9 MB (67.94%) - LeetHub

--- 0942-di-string-match/0942-di-string-match.java
+++ 0942-di-string-match/0942-di-string-match.java
@@ -4,13 +4,8 @@ class Solution {
 		int min = 0;
 		int[] arr = new int[max + 1];
 		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == 'I') {
-				arr[i] = min++;
-			} else if (s.charAt(i) == 'D') {
-				arr[i] = max--;
-			} else {
-				return null;
-			}
+            arr[i] = s.charAt(i) == 'I' ? min++ : max--;
+			
 		}
 		
 			arr[s.length()] = min;
