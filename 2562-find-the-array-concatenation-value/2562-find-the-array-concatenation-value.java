0f58808e2b5245c56b2726b1fc7bf89dee5c48e1
JotheeshKumar <80279461+jo73na@users.noreply.github.com>
2024-01-10 21:47:23 -0500

    Time: 4 ms (51.73%), Space: 44.4 MB (13.65%) - LeetHub

--- 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.java
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.java
@@ -9,13 +9,13 @@ class Solution {
             int num = concatenate(nums[i++],nums[j--]);
             res += num;
         }
-        if((nums.length&1) != 0){
-            res += nums[i];
+        if(nums.length%2!=0){
+            res += nums[nums.length/2];
         }
         return res;
     }
     private int concatenate(int x,int y){
-        return Integer.parseInt(x+""+y);
+        return Integer.parseInt(String.valueOf(x)+String.valueOf(y));
     }
 
     }
